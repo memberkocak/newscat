c96de3edbaf6f3f78840618d517a68c0eb8de45e
shadowbot82 <42908347+shadowbot82@users.noreply.github.com>
2018-09-22 06:38:56 +0700

    Update cat.php

--- cat.php
+++ cat.php
@@ -1,12 +1,12 @@
 <?php
-// FB : Manggala Febri Valentino
-// Not For Sale 
-// THX TO SGB TEAM
-function cat($code, $key, $token, $jumlah, $wait){
+// @author : Shadow Bot
+// Tidak Untuk Di Jual
+// Gunakan Dengan Bijak
+function cat($token, $jumlah, $wait){
     $x = 0; 
     while($x < $jumlah) {
 		
-		$body = 'sgbcode='.$code.'&sgbsecret='.$key.'&token='.$token.'';
+		$body = 'token='.$token.'';
 				
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL,"https://api-siptruk.c9users.io/api.php");
@@ -24,17 +24,17 @@ function cat($code, $key, $token, $jumlah, $wait){
     }
 }
 print "TUYUL COIN NEWS CAT\n\n";
-echo "SGB CODE?\nInput : ";
-$code = trim(fgets(STDIN));
-echo "SGB SECRET KEY?\nInput : ";
-$key = trim(fgets(STDIN));
+
+
+
+
 echo "TOKEN?\nInput : ";
 $token = trim(fgets(STDIN));
 echo "Jumlah?\nInput : ";
 $jumlah = trim(fgets(STDIN));
 echo "Jeda? 0-9999999999 (ex:0)\nInput : ";
 $wait = trim(fgets(STDIN));
-$execute = cat($code, $key, $token, $jumlah, $wait);
+$execute = cat($token, $jumlah, $wait);
 print $execute;
 print "DONE ALL SEND\n";
 ?>
